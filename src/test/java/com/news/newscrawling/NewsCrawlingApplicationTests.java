07cbf3d671b2409ffc86939b87fa6ba758a3e711
alfhaos <alfhaos@naver.com>
2025-08-27 21:00:02 +0900

    최초 생성

---
+++ src/test/java/com/news/newscrawling/NewsCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.newscrawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsCrawlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
